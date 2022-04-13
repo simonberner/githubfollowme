513f8ae3b01494f44faa245de774d3e82f8c323e
Simon Berner <sibebzh@gmail.com>
2022-04-13 09:30:12 +0200

    init: :tada: initialize the project

---
+++ GitHubFollowMe/GitHubFollowMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubFollowMeApp.swift
+//  GitHubFollowMe
+//
+//  Created by Simon Berner on 12.04.22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubFollowMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
