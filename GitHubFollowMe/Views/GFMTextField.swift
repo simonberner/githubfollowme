cf09b17c159b4410bdb0f44640fe81347724cfca
Simon Berner <sibebzh@gmail.com>
2022-04-19 13:14:22 +0200

    add(textfield): :sparkles: add a generic GFMTextField

---
+++ GitHubFollowMe/Views/GFMTextField.swift
@@ -0,0 +1,31 @@
+//
+//  GFMTextField.swift
+//  GitHubFollowMe
+//
+//  Created by Simon Berner on 18.04.22.
+//
+
+import SwiftUI
+
+struct GFMTextField: View {
+    var placeholderText: String
+    var textInput: Binding<String>
+
+    var body: some View {
+        TextField(placeholderText, text: textInput)
+            .textFieldStyle(.roundedBorder)
+            .disableAutocorrection(true)
+            .font(.title2)
+//            .font(.system(size: 20, weight: .bold))
+            .overlay(RoundedRectangle(cornerRadius: 10).strokeBorder(.gray, style: StrokeStyle(lineWidth: 2)))
+            .padding()
+            .scaledToFit()
+            .multilineTextAlignment(.center)
+    }
+}
+
+struct GFMTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        GFMTextField(placeholderText: "Enter a Username", textInput: .constant(""))
+    }
+}
