4be9158853c06ff9ae6a79db8a8096c48c7992ce
Simon Berner <sibebzh@gmail.com>
2022-05-31 21:26:04 +0200

    add(follower): :boom: add some MockData

--- GitHubFollowMe/Model/Follower.swift
+++ GitHubFollowMe/Model/Follower.swift
@@ -7,8 +7,18 @@
 
 import Foundation
 
-struct Follower: Codable {
+struct Follower: Codable, Identifiable {
+    var id = UUID()
     // Not optional as they will exist in every GitHub profile
     var login: String
     var avatarUrl: String
+}
+
+struct MockData {
+    static let sampleFollower1 = Follower(id: UUID(), login: "user1", avatarUrl: "")
+    static let sampleFollower2 = Follower(id: UUID(), login: "user2", avatarUrl: "")
+    static let sampleFollower3 = Follower(id: UUID(), login: "user3", avatarUrl: "")
+    static let sampleFollower4 = Follower(id: UUID(), login: "user4", avatarUrl: "")
+
+    static let followers = [sampleFollower1, sampleFollower2, sampleFollower3, sampleFollower4]
 }
