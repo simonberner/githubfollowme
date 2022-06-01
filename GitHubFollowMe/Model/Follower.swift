b3552872565f0aae412227724260664e006ffbf9
Simon Berner <sibebzh@gmail.com>
2022-06-01 08:37:34 +0200

    fix(follower): :bug: change id to an Int
    Change the id to an Int so that a Follower can be decoded from the
    JSON resonse.

--- GitHubFollowMe/Model/Follower.swift
+++ GitHubFollowMe/Model/Follower.swift
@@ -8,17 +8,17 @@
 import Foundation
 
 struct Follower: Codable, Identifiable {
-    var id = UUID()
+    var id: Int
     // Not optional as they will exist in every GitHub profile
     var login: String
     var avatarUrl: String
 }
 
 struct MockData {
-    static let sampleFollower1 = Follower(id: UUID(), login: "user1", avatarUrl: "")
-    static let sampleFollower2 = Follower(id: UUID(), login: "user2", avatarUrl: "")
-    static let sampleFollower3 = Follower(id: UUID(), login: "user3", avatarUrl: "")
-    static let sampleFollower4 = Follower(id: UUID(), login: "user4", avatarUrl: "")
+    static let sampleFollower1 = Follower(id: 1, login: "user1", avatarUrl: "")
+    static let sampleFollower2 = Follower(id: 2, login: "user2", avatarUrl: "")
+    static let sampleFollower3 = Follower(id: 3, login: "user3", avatarUrl: "")
+    static let sampleFollower4 = Follower(id: 4, login: "user4", avatarUrl: "")
 
     static let followers = [sampleFollower1, sampleFollower2, sampleFollower3, sampleFollower4]
 }
