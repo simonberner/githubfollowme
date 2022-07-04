77c986911f3655bb415c19ead8845aa7339f5e9e
Simon Berner <sibebzh@gmail.com>
2022-07-04 17:17:19 +0200

    add(view): :boom: add UserDetailView

--- GitHubFollowMe/Screens/UserDetailView.swift
+++ GitHubFollowMe/Screens/UserDetailView.swift
@@ -9,24 +9,33 @@ import SwiftUI
 
 struct UserDetailView: View {
 
+//    @EnvironmentObject var viewModel: GFMViewModel
+
     var body: some View {
 
-        VStack {
+        VStack(alignment: .leading, spacing: 14) {
             HStack {
-                // user info
-                AvatarImageView(imageURL: viewModel.selectedUser?.avatarUrl, imageWidth: 86, imageHeight: 86)
-                VStack {
-                    // VStack
-                    // login
-                    // name
-                    // symbol and location
+                AvatarImageView(imageURL: URL(string: UserMockData.mockUser.avatarUrl), imageWidth: 86, imageHeight: 86)
+                VStack(alignment: .leading, spacing: 3) {
+                    Text(UserMockData.mockUser.login)
+                        .bold()
+                        .font(.title)
+                    Text(UserMockData.mockUser.name ?? "")
+                        .foregroundColor(.secondary)
+                    Label {
+                        Text(UserMockData.mockUser.location ?? "")
+                    } icon: {
+                        Image(systemName: "mappin.and.ellipse")
+                    }
+                    .foregroundColor(.secondary)
                 }
             }
-            // Text with the bio
-            // section with GitHub Profile button
-            // section with the Get Followers button
-            // Text: GitHub since Jul 2014
+            Text(UserMockData.mockUser.bio ?? "")
+                .font(.caption)
+                .lineLimit(3)
+                .foregroundColor(.secondary)
         }
+        .padding(12)
 
 //        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
 //            .overlay(GFMDismissButton(), alignment: .topTrailing)
