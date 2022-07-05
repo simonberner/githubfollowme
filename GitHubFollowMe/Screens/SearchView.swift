e602a4c418f77b3dc0f4fa4eff8f45f24e34afd1
Simon Berner <sibebzh@gmail.com>
2022-07-05 21:46:04 +0200

    refactor(searchview): :hammer: add the viewModel as EnvironmentObject

--- GitHubFollowMe/Screens/SearchView.swift
+++ GitHubFollowMe/Screens/SearchView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SearchView: View {
 
     @FocusState private var focusedTextField: SearchFieldEnum?
-    @StateObject var viewModel = GFMViewModel() // this view owns the viewModel
+    @EnvironmentObject var viewModel: GFMViewModel
 
     var body: some View {
         // Will be deprecated in iOS16 -> https://developer.apple.com/documentation/swiftui/navigationview?changes=latest_minor
@@ -18,7 +18,7 @@ struct SearchView: View {
         NavigationView {
             VStack {
                 NavigationLink("", destination: FollowersView(), isActive: $viewModel.showFollowersView)
-//                    .navigationTitle(viewModel.showFollowersView ? "Search" : "")
+                //                    .navigationTitle(viewModel.showFollowersView ? "Search" : "")
                 Image("gh-logo")
                     .resizable()
                     .frame(width: 240, height: 240)
@@ -39,8 +39,7 @@ struct SearchView: View {
             } message: {
                 Text(viewModel.alertItem?.message ?? "")
             }
-        // attach the viewModel to the NavigationView so that it will be available in its child views (NavigationLink)
-        }.environmentObject(viewModel)
+        }
     }
 }
 
