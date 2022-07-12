39de65240ced95cd7fda80a2b6b8ecaaf1aaa4b1
Simon Berner <sibebzh@gmail.com>
2022-07-12 18:57:12 +0200

    test(date): :white_check_mark: add a test for formatting date
    Add a test for formatting the GitHub profile createdAt date.

---
+++ GitHubFollowMeTests/GFMDateFormatTests.swift
@@ -0,0 +1,35 @@
+//
+//  GFMDateFormatterTests.swift
+//  GitHubFollowMeTests
+//
+//  Created by Simon Berner on 06.07.22.
+//
+
+import XCTest
+@testable import GitHubFollowMe
+
+class GFMDateFormatTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        super.setUp()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testGitHubCreationDateFormatter() throws {
+        let createAtDate = "2022-07-07T12:00:00Z" // Z: Zulu time zone is UTC +0 -> Jul 7, 2022
+        let formattedDate = createAtDate.formatDate()
+        print("formattedDate: \(formattedDate)")
+        XCTAssertTrue(formattedDate == "7. Juli 2022")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
